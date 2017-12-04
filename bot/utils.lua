2238fff1ff6b7cff30a1a1a6e6efb4182fc30b72
kazem199700 <34087957+kazem199700@users.noreply.github.com>
2017-12-05 00:07:53 +0300

    Update utils.lua

--- bot/utils.lua
+++ bot/utils.lua
@@ -136,7 +136,7 @@ end
   if not lang then
     return tdcli.sendMessage(msg.to.id, msg.id, 0, "_All group admins has been promoted and group creator is now group owner_", 0, "md")
 else
-    return tdcli.sendMessage(msg.to.id, msg.id, 0, "_تمام ادمین های گروه به مقام مدیر منتصب شدند و سازنده گروه به مقام مالک گروه منتصب شد_", 0, "md")
+    return tdcli.sendMessage(msg.to.id, msg.id, 0, "_تم تعيين جميع مديري المجموعة لمنصب المدير وتم تعيين المجموعة كمالك للمجموعة_", 0, "md")
      end
  end
 tdcli.getChannelMembers(msg.to.id, 0, 'Administrators', 200, config_cb, {chat_id=msg.to.id})
@@ -729,7 +729,7 @@ local lang = redis:get(hash)
   if not lang then
     return '_Group is not added_'
 else
-    return 'گروه به لیست گروه های مدیریتی ربات اضافه نشده است'
+    return 'لا يتم إضافة المجموعة إلى قائمة مجموعات إدارة الروبوت'
    end
   end
   -- determine if table is empty
@@ -737,13 +737,13 @@ else
      if not lang then
 					return "_No_ *banned* _users in this group_"
    else
-					return "*هیچ کاربری از این گروه محروم نشده*"
+					return "*لم تتم إزالة أي مستخدمين من هذه المجموعة*"
               end
 				end
        if not lang then
    message = '*List of banned users :*\n'
          else
-   message = '_لیست کاربران محروم شده از گروه :_\n'
+   message = '_قائمة بالمستخدمين المحرومين من المجموعة :_\n'
      end
   for k,v in pairs(data[tostring(chat_id)]['banned']) do
     message = message ..i.. '- '..v..' [' ..k.. '] \n'
@@ -761,7 +761,7 @@ local lang = redis:get(hash)
   if not lang then
     return '_Group is not added_'
 else
-    return 'گروه به لیست گروه های مدیریتی ربات اضافه نشده است'
+    return 'لا يتم إضافة المجموعة إلى قائمة مجموعات إدارة الروبوت'
    end
   end
   -- determine if table is empty
@@ -769,13 +769,13 @@ else
         if not lang then
 					return "_No_ *silent* _users in this group_"
    else
-					return "*لیست کاربران سایلنت شده خالی است*"
+					return "*قائمة المستخدمين الصامتين فارغة*"
              end
 				end
       if not lang then
    message = '*List of silent users :*\n'
        else
-   message = '_لیست کاربران سایلنت شده :_\n'
+   message = '_قائمة المستخدمين الصامتين :_\n'
     end
   for k,v in pairs(data[tostring(chat_id)]['is_silent_users']) do
     message = message ..i.. '- '..v..' [' ..k.. '] \n'
@@ -793,7 +793,7 @@ local lang = redis:get(hash)
   if not lang then
     return '_Group is not added_'
 else
-    return 'گروه به لیست گروه های مدیریتی ربات اضافه نشده است'
+    return 'لا يتم إضافة المجموعة إلى قائمة مجموعات إدارة الروبوت'
    end
   end
   if not data[tostring(chat_id)]['whitelist'] then
@@ -805,13 +805,13 @@ else
      if not lang then
 					return "_No_ *users* _in white list_"
    else
-					return "*هیچ کاربری در لیست سفید وجود ندارد*"
+					return "*لا يوجد مستخدمون في القائمة البيضاء*"
               end
 				end
        if not lang then
    message = '*Users of white list :*\n'
          else
-   message = '_کاربران لیست سفید :_\n'
+   message = '_المستخدمون في القائمة البيضاء :_\n'
      end
   for k,v in pairs(data[tostring(chat_id)]['whitelist']) do
     message = message ..i.. '- '..v..' [' ..k.. '] \n'
@@ -833,13 +833,13 @@ local lang = redis:get(hash)
     if not lang then
 					return "_No_ *globally banned* _users available_"
    else
-					return "*هیچ کاربری از گروه های ربات محروم نشده*"
+					return "*لم يتم تجريد أي مستخدم من مجموعات الروبوت*"
              end
 				end
         if not lang then
    message = '*List of globally banned users :*\n'
    else
-   message = '_لیست کاربران محروم شده از گروه های ربات :_\n'
+   message = '_قائمة بالمستخدمين المحرومين من مجموعات الروبوت :_\n'
    end
   for k,v in pairs(data['gban_users']) do
     message = message ..i.. '- '..v..' [' ..k.. '] \n'
@@ -860,7 +860,7 @@ local lang = redis:get(hash)
   if not lang then
     return '_Group is not added_'
 else
-    return 'گروه به لیست گروه های مدیریتی ربات اضافه نشده است'
+    return 'لا يتم إضافة المجموعة إلى قائمة مجموعات إدارة الروبوت'
    end
   end
   -- determine if table is empty
@@ -868,7 +868,7 @@ else
       if not lang then
     return "*Filtered words list* _is empty_"
       else
-    return "_لیست کلمات فیلتر شده خالی است_"
+    return "_قائمة الكلمات التي تمت تصفيتها فارغة_"
      end
   end
   if not data[tostring(msg.chat_id_)]['filterlist'] then
@@ -878,7 +878,7 @@ else
       if not lang then
        filterlist = '*List of filtered words :*\n'
          else
-       filterlist = '_لیست کلمات فیلتر شده :_\n'
+       filterlist = '_قائمة الكلمات التي تمت تصفيتها:_\n'
     end
  local i = 1
    for k,v in pairs(data[tostring(msg.chat_id_)]['filterlist']) do
